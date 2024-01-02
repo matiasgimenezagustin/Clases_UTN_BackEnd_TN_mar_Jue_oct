60eebc584461837772fa47792c3e8e97ba340019
matu.dev <matu.a.gimenez2002@gmail.com>
2024-01-02 20:29:20 -0300

    Todo

--- clase-19 -20 - 21 - 22 - 23/frontend/src/components/Home/Home.jsx
+++ clase-19 -20 - 21 - 22 - 23/frontend/src/components/Home/Home.jsx
@@ -23,20 +23,28 @@ const Home = () => {
     }, [])
   return (
     <div>
-        <h1>Lista de productos</h1>
-        {products.length == 0 
-        ? <h2>Cargando..</h2>
-        : products...}
-        {/* 
-        Renderizar a partir del estado products los productos en divs 
 
-        <h2>Nombre</h2>
-        <p>Precio: $</p>
-        <span>Stock: stock</span>
-        <button>Ver detalle</button>
-        */}
+        <div>
+        <h1>Lista de productos</h1>
+        {products.length == 0 ? <h2>Cargando...</h2> : products.map(product =>
+            <Product key={product.id} {...product}/>
+        )}
+    </div>
+  )
     </div>
   )
 }
 
 export default Home
+
+const Product = ({nombre, precio, stock, id}) =>{
+    return (
+        <div >
+            <h2>Nombre {nombre}</h2>
+            <p>Precio {precio}</p>
+            <span>Stock: {stock}</span>
+            <br/>
+            <button>Ver Detalle </button>
+        </div>
+    )
+}
